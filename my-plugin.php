705b0d18c9988ec37e3aa2e09799ee15b0721811
davidpom <david@pom.es>
2016-07-27 16:01:21 +0200

    papapa

--- my-plugin.php
+++ my-plugin.php
@@ -9,3 +9,10 @@ Author URI: http://david.be/
 Domain Path: localization/
 Text Domain: my-plugin
 */
+
+include_once plugin_dir_path( __FILE__ ).'updater.php';
+$updater = new My_Plugin_Updater(__FILE__);
+$updater->set_username('davidpom');
+	$updater->set_repository('my-plugin');
+	//$updater->authorize('6c1cd9991dafa751f7fdbd59324317c75eee39de');
+	$updater->initialize();
