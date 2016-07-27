9d4db9202274893b33e6c22fc0f835d73fe72bae
davidpom <david@pom.es>
2016-07-27 14:56:00 +0200

    fefawef

---
+++ my-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: My PLugin
+Plugin URI: http://pom.es
+Description: 
+Version: 1.0.0
+Author: David
+Author URI: http://david.be/
+Domain Path: localization/
+Text Domain: my-plugin
+*/
